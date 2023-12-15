77189384bb39a702169e175cc30fbf1fee851f07
wiylanbellache <bellachewiylan@gmail.com>
2023-12-15 11:36:42 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'i9L9glyYcZB7ThQZ', // TODO
+    secretAccessKey: 'oISccRkZWvetUm39IT0DqcDmR3ifOJEtAVK0OT3d', // TODO
+    endpoint: 's3.tebi.io', // TODO
+    bucket: 'esgi-cloud-exam-bucket-33770', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
